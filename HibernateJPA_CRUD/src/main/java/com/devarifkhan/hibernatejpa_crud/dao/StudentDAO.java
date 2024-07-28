dc93d1564f35844905385bb27dd78e0e2581d9d7
devarifkhan <arifcse209@gmail.com>
2024-07-28 16:08:20 +0600

    querying objects with jpa

--- HibernateJPA_CRUD/src/main/java/com/devarifkhan/hibernatejpa_crud/dao/StudentDAO.java
+++ HibernateJPA_CRUD/src/main/java/com/devarifkhan/hibernatejpa_crud/dao/StudentDAO.java
@@ -2,8 +2,12 @@ package com.devarifkhan.hibernatejpa_crud.dao;
 
 import com.devarifkhan.hibernatejpa_crud.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 }
