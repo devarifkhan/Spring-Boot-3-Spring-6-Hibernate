3bb1d591ca71e13cceb1a53c2c042e12fff91ec0
devarifkhan <arifcse209@gmail.com>
2024-07-29 10:52:44 +0600

    initial setup rest crud apis

---
+++ REST CRUD APIs/src/main/java/com/devarifkhan/rest_crud_apis/RestCrudApIsApplication.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.rest_crud_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudApIsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCrudApIsApplication.class, args);
+    }
+
+}
