6e7232ca8f6a91430c8f3259a52ea000c15d7cc5
devarifkhan <arifcse209@gmail.com>
2024-07-29 12:12:22 +0600

    rest DAO

---
+++ Rest Crud Employee/src/main/java/com/devarifkhan/rest_crud_employee/RestCrudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.rest_crud_employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCrudEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCrudEmployeeApplication.class, args);
+    }
+
+}
---
+++ Rest Crud Employee/src/test/java/com/devarifkhan/rest_crud_employee/RestCrudEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.rest_crud_employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
