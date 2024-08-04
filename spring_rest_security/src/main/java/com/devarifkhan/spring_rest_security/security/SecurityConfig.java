936ceb5c928a4996eada275cce287ebd1e6551f8
devarifkhan <arifcse209@gmail.com>
2024-08-04 18:59:05 +0600

    basic configuration

---
+++ spring_rest_security/src/main/java/com/devarifkhan/spring_rest_security/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.devarifkhan.spring_rest_security.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails john = User.builder()
+                .username("john")
+                .password("{noop}12345")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("mary")
+                .password("{noop}12345")
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("susan")
+                .password("{noop}12345")
+                .roles("EMPLOYEE", "MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
