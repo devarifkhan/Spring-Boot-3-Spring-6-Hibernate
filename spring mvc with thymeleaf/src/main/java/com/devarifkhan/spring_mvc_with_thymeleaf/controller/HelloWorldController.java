c48841bc4e114d952790e7a10ed40b125a43f135
devarifkhan <arifcse209@gmail.com>
2024-08-06 16:39:48 +0600

    mvc binding request params

--- spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/HelloWorldController.java
+++ spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/HelloWorldController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -23,13 +24,28 @@ public class HelloWorldController {
     // need a controller method to read form data and
     // add data to the model
     @RequestMapping("/processFormV2")
-    public String letsShoutDude(HttpServletRequest request, Model model) {
+    public String processFormV2(HttpServletRequest request, Model model) {
         // read the request parameter from the HTML form
         String theName = request.getParameter("studentName");
         // convert the data to all caps
         theName = theName.toUpperCase();
         // create the message
         String result="Yo! " + theName;
+        // add the name to the model
+        model.addAttribute("message", result.toUpperCase());
+
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormV3")
+    public String processFormV3(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+        // create the message
+        String result="Hey My Friend! " + theName;
         // add the name to the model
         model.addAttribute("message", result.toUpperCase());
 
