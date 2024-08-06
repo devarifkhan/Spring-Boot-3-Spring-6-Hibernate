bb4a7637ba13e3407ef4d44f942d03d7bb65e3a4
devarifkhan <arifcse209@gmail.com>
2024-08-06 16:32:00 +0600

    add data to spring mvc model

--- spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/HelloWorldController.java
+++ spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.devarifkhan.spring_mvc_with_thymeleaf.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +17,22 @@ public class HelloWorldController {
     // need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormV2")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+        // create the message
+        String result="Yo! " + theName;
+        // add the name to the model
+        model.addAttribute("message", result.toUpperCase());
+
         return "helloworld";
     }
 }
