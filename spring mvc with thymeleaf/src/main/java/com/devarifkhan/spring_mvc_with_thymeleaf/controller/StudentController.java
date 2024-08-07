8f857649b0a0b968e99c4e555b9dac9b1f762978
devarifkhan <arifcse209@gmail.com>
2024-08-07 16:04:12 +0600

    got value from application.properties

--- spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/StudentController.java
+++ spring mvc with thymeleaf/src/main/java/com/devarifkhan/spring_mvc_with_thymeleaf/controller/StudentController.java
@@ -1,14 +1,25 @@
 package com.devarifkhan.spring_mvc_with_thymeleaf.controller;
 
 import com.devarifkhan.spring_mvc_with_thymeleaf.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
@@ -16,6 +27,9 @@ public class StudentController {
         Student student = new Student();
         // add student object to the model
         theModel.addAttribute("student", student);
+        theModel.addAttribute("countries", countries);
+        theModel.addAttribute("languages", languages);
+        theModel.addAttribute("systems", systems);
         return "student-form";
     }
 
