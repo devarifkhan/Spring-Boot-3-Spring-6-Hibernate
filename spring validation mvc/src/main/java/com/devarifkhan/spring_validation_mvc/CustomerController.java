c6755ed3dbd31b6396904649386f9a4c45461ffa
devarifkhan <arifcse209@gmail.com>
2024-08-07 16:32:49 +0600

    spring mvc validation init binder

---
+++ spring validation mvc/src/main/java/com/devarifkhan/spring_validation_mvc/CustomerController.java
@@ -0,0 +1,40 @@
+package com.devarifkhan.spring_validation_mvc;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+    // add an init binder ... to convert trim the input string
+    // remove the trailing whitespace
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    // Add code here
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());  // Create a new Customer object and add it to the model
+        return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        return "customer-confirmation";
+    }
+
+}
---
+++ spring validation mvc/src/test/java/com/devarifkhan/spring_validation_mvc/SpringValidationMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.spring_validation_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
