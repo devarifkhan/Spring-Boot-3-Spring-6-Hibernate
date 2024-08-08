522102605d7d2ef0ee4a73489e4f685c3eb4e4fb
devarifkhan <arifcse209@gmail.com>
2024-08-08 17:03:07 +0600

    basic configuration

---
+++ spring_mvc_security/src/main/java/com/devarifkhan/spring_mvc_security/controller/DController.java
@@ -0,0 +1,13 @@
+package com.devarifkhan.spring_mvc_security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DController {
+
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
