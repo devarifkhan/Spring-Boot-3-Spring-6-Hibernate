6fff823f09a62cefa85e24ca3b28cf47616b4dba
devarifkhan <arifcse209@gmail.com>
2024-08-08 16:38:46 +0600

    update and delete operation employee

--- spring_mvc_crud/src/main/java/com/devarifkhan/spring_mvc_crud/controller/EmployeeController.java
+++ spring_mvc_crud/src/main/java/com/devarifkhan/spring_mvc_crud/controller/EmployeeController.java
@@ -5,7 +5,9 @@ import com.devarifkhan.spring_mvc_crud.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -24,6 +26,36 @@ public class EmployeeController {
     public String listEmployees(Model theModel) {
         List<Employee> theEmployees = employeeService.findAll();
         theModel.addAttribute("employees", theEmployees);
-        return "list-employees";
+        return "employees/list-employees";
     }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel) {
+        Employee theEmployee = new Employee();
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    // add mapping for "/save"
+    @PostMapping("/save")
+    public String saveEmployee(Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
+    }
+
+    // add mapping for "/showFormForUpdate"
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("id") int theId, Model theModel) {
+        Employee theEmployee = employeeService.findById(theId);
+        theModel.addAttribute("employee", theEmployee);
+        return "employees/employee-form";
+    }
+
+    // add mapping for "/delete"
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") int theId) {
+        employeeService.deleteById(theId);
+        return "redirect:/employees/list";
+    }
+
 }
