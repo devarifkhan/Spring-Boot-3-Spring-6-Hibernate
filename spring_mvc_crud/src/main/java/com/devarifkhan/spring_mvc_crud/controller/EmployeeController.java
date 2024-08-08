6e0fdedfeeeb3b7ffaf45e1c57b4249881b87087
devarifkhan <arifcse209@gmail.com>
2024-08-08 15:58:52 +0600

    get employees controller setup

---
+++ spring_mvc_crud/src/main/java/com/devarifkhan/spring_mvc_crud/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.devarifkhan.spring_mvc_crud.controller;
+
+import com.devarifkhan.spring_mvc_crud.entity.Employee;
+import com.devarifkhan.spring_mvc_crud.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    //constructor innjection
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees";
+    }
+}
