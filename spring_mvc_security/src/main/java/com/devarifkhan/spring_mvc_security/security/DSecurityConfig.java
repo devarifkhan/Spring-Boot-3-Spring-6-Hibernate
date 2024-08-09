c581e39cc0e3c508577666162ffbb95e758d0c02
devarifkhan <arifcse209@gmail.com>
2024-08-09 09:37:00 +0600

    user name and role in html

--- spring_mvc_security/src/main/java/com/devarifkhan/spring_mvc_security/security/DSecurityConfig.java
+++ spring_mvc_security/src/main/java/com/devarifkhan/spring_mvc_security/security/DSecurityConfig.java
@@ -3,6 +3,7 @@ package com.devarifkhan.spring_mvc_security.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -45,6 +46,8 @@ public class DSecurityConfig {
                                 form.loginPage("/showMyLoginPage")
                                         .loginProcessingUrl("/authenticateTheUser")
                                         .permitAll()
+                ).logout(
+                        LogoutConfigurer::permitAll
                 );
 
         return http.build();
