d9b7156af81027b31b0e00bc0809ac9be21809ca
devarifkhan <arifcse209@gmail.com>
2024-08-15 16:13:18 +0600

    aop read method arguments with joinpoints

--- aspect_oriented_programming/src/main/java/com/devarifkhan/aspect_oriented_programming/aspect/MyLoggingAspect.java
+++ aspect_oriented_programming/src/main/java/com/devarifkhan/aspect_oriented_programming/aspect/MyLoggingAspect.java
@@ -1,10 +1,13 @@
 package com.devarifkhan.aspect_oriented_programming.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
 
 @Aspect
 @Component
@@ -12,18 +15,19 @@ import org.springframework.stereotype.Component;
 public class MyLoggingAspect {
 
 
-
-
-
-
-
-    //    @Before("execution(public void add*())")
-    //    @Before("execution(* com.devarifkhan.aspect_oriented_programming.dao.*.*(..))")
-
     @Before("com.devarifkhan.aspect_oriented_programming.aspect.DevArifAopExpressions.forDaoPackage()")
-    public void beforeAddAccountAdvice() {
+    public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
+        // display the method signature
         System.out.println("\n=====>>> Executing @Before advice on addAccount()");
+        MethodSignature methodSignature=(MethodSignature) theJoinPoint.getSignature();
+        System.out.println("Method: "+methodSignature);
+        // display method arguments
+        Object[] args=theJoinPoint.getArgs();
     }
+
+
+
+
 
 
 
